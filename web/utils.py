e8c8f4275d2747d0bd18bdd82170158d1124a861
Eddy Almuiña <almuina.e@gmail.com>
2022-09-10 02:37:21 +0200

    Removed debug line

--- web/utils.py
+++ web/utils.py
@@ -5,8 +5,6 @@ from ipware import get_client_ip
 
 
 def get_timezone(request):
-    print(request.META)
-
     client_ip, is_routable = get_client_ip(request)
     ipinfo_token = os.environ.get('IPINFO_TOKEN')
 
